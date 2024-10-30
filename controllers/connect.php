244ba3ac1075dbe75015f2b1063535ed8e285fda
lcrgn <120507186+lcrgn@users.noreply.github.com>
2024-10-30 11:24:25 +0100

    updateUserConnect

--- controllers/connect.php
+++ controllers/connect.php
@@ -11,14 +11,14 @@ class Connect{
     public function connection(){
         // dans le cas ou le champ user est vide -> je met les caractères spéciaux en html 
         // et je précise que si le champ "user" est vide : un message précisant à l'utilisateur de remplir ce champ
-        if(empty(htmlspecialchars($_POST['user']))){
+        if(empty(htmlspecialchars($_POST['pseudo']))){
             // affichage du message d'erreur dans le cas ou le champ reste vide 
             throw new \Exception("Veuillez entrer un nom d'utilisateur valide");
         }
         else{
             // dans le cas ou le champ n'est pas vide alors nous prenons l'user
             // filter_var = pour filtrer une variable pour nettoyer les données
-            $user = htmlspecialchars($_POST['user']);
+            $pseudo = htmlspecialchars($_POST['pseudo']);
         }
         // même cas de sécurité que pour l'user -> le mot de passe ne doit pas être vide 
         // sinon affichage du message d'erreur
@@ -28,7 +28,7 @@ class Connect{
             $password = htmlspecialchars($_POST['password']);
 
         $userModel = new User();
-        $resultat = $userModel->userConnect($user, $password);
+        $resultat = $userModel->userConnect($pseudo, $password);
         $_SESSION['connect'] = 1;
         $_SESSION['pseudo'] = $resultat['pseudo'];
         $_SESSION['id'] = $resultat['id'];
