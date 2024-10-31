ac4d2bbc8a53b146ed087aa9b28ce5924ed8a2b7
justindev116 <justin.huicq@mediaschool.me>
2024-10-31 14:58:18 +0100

    index

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+
+require_once '../controllers/connect.php';
+require_once '../controllers/deconnect.php';
+require_once '../controllers/inscription.php';
+require_once '../controllers/profil.php';
